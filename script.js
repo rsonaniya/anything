9f006bb81e3d59881fca5135a300d6351715fcf4
rsonaniya <rajatsonaniya28@gmail.com>
2023-09-06 12:38:14 +0530

    created 2 boxes

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let box = document.getElementById("box");
-box.addEventListener("click",()=>{
-alert("box has been clicked")
+let box1 = document.getElementById("box1");
+box1.addEventListener("click",()=>{
+alert("box1 has been clicked")
+})
+let box2 = document.getElementById("box2");
+box2.addEventListener("click",()=>{
+alert("box2 has been clicked")
 })
